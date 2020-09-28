8dcf7d7a917bf586c5e57daa111aba509b5ad9bd
Andrew Hart <a.p.hart97@gmail.com>
2020-09-28 19:05:11 -0400

    Update Configuration.h
    Updated temp for PLA and probe offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -995,7 +995,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -42, -10, -2.21 }
+#define NOZZLE_TO_PROBE_OFFSET { -42, -10, -2.28 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1512,7 +1512,7 @@
 
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 200
+#define PREHEAT_1_TEMP_HOTEND 195
 #define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
