561da5c8afb15f8a2baee362c3896f3d064e28b5
Andrew Hart <a.p.hart97@gmail.com>
2020-10-08 15:02:21 -0400

    Update Configuration.h
    Update min temp

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -561,7 +561,7 @@
  * *** IT IS HIGHLY RECOMMENDED TO LEAVE THIS OPTION ENABLED! ***
  */
 #define PREVENT_COLD_EXTRUSION
-#define EXTRUDE_MINTEMP 180
+#define EXTRUDE_MINTEMP 175
 
 /**
  * Prevent a single extrusion longer than EXTRUDE_MAXLENGTH.
